69376b6b9854aef044f683aaaae655a6c2f13232
k4hfie <1adacchi@gmail.com>
2026-06-28 21:38:46 +0700

    added color library

---
+++ include/color.h
@@ -0,0 +1,14 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#define HBLK "\e[0;90m"
+#define HRED "\e[0;91m"
+#define HGRN "\e[0;92m"
+#define HYEL "\e[0;93m"
+#define HBLU "\e[0;94m"
+#define HMAG "\e[0;95m"
+#define HCYN "\e[0;96m"
+#define HWHT "\e[0;97m"
+#define RESET "\e[0m"
+
+#endif
